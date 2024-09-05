0a23009a52a40da097157aae4321b49f1411c4b6
juliafabri <137795108+juliafabri@users.noreply.github.com>
2024-09-05 18:09:29 -0300

    Add files via upload

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,24 +1,34 @@
-function notifyMe() {
-    if (!("Notification" in window)) {
-      // Check if the browser supports notifications
-      alert("This browser does not support desktop notification");
-    } else if (Notification.permission === "granted") {
-      // Check whether notification permissions have already been granted;
-      // if so, create a notification
-      const notification = new Notification("Hi there!");
-      // …
-    } else if (Notification.permission !== "denied") {
-      // We need to ask the user for permission
-      Notification.requestPermission().then((permission) => {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-          const notification = new Notification("Hi there!");
-          // …
-        }
+// function notifyMe() {
+//     if (!("Notification" in window)) {
+//         Notification.requestPermission();
+//       // Check if the browser supports notifications
+//       alert("This browser does not support desktop notification");
+//     } else if (Notification.permission === "granted") {
+//       // Check whether notification permissions have already been granted;
+//       // if so, create a notification
+//       const notification = new Notification("Hi there!");
+//       // …
+//     } else if (Notification.permission !== "denied") {
+//       // We need to ask the user for permission
+//       Notification.requestPermission().then((permission) => {
+//         // If the user accepts, let's create a notification
+//         if (permission === "granted") {
+//           const notification = new Notification("Hi there!");
+//           // …
+//         }
+//       });
+//     }
+//   }
+  
+function enviarN(){
+    Notification.requestPermission();
+    self.addEventListener('push', (event) => {
+        event.waitUntil(
+          self.registration.showNotification('Notification Title', {
+            body: 'Notification Body Text',
+            icon: 'custom-notification-icon.png',
+          });
+        );
       });
-    }
-  
-    // At last, if the user has denied notifications, and you
-    // want to be respectful there is no need to bother them anymore.
-  }
-  
+      
+}
