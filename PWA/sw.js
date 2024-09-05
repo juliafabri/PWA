22f9898979a2cc93d37a2f172bbe0c81c4ff5b9c
juliafabri <137795108+juliafabri@users.noreply.github.com>
2024-09-05 17:34:11 -0300

    Add files via upload

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,18 +1,18 @@
-function enviarN() {
+function notifyMe() {
     if (!("Notification" in window)) {
       // Check if the browser supports notifications
-      alert("Esse browser não suporta notificações!");
+      alert("This browser does not support desktop notification");
     } else if (Notification.permission === "granted") {
       // Check whether notification permissions have already been granted;
       // if so, create a notification
-      const notification = new Notification("Isso é uma notificação!");
+      const notification = new Notification("Hi there!");
       // …
     } else if (Notification.permission !== "denied") {
       // We need to ask the user for permission
       Notification.requestPermission().then((permission) => {
         // If the user accepts, let's create a notification
         if (permission === "granted") {
-          const notification = new Notification("Isso é uma notificação!");
+          const notification = new Notification("Hi there!");
           // …
         }
       });
@@ -21,3 +21,4 @@ function enviarN() {
     // At last, if the user has denied notifications, and you
     // want to be respectful there is no need to bother them anymore.
   }
+  
