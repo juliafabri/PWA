8a93d138ece64602859b9360f3f8a32a892d2ca9
juliafabri <137795108+juliafabri@users.noreply.github.com>
2024-09-05 18:16:52 -0300

    Add files via upload

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,33 +1,38 @@
-// function notifyMe() {
-//     if (!("Notification" in window)) {
-//         Notification.requestPermission();
-//       // Check if the browser supports notifications
-//       alert("This browser does not support desktop notification");
-//     } else if (Notification.permission === "granted") {
-//       // Check whether notification permissions have already been granted;
-//       // if so, create a notification
-//       const notification = new Notification("Hi there!");
-//       // …
-//     } else if (Notification.permission !== "denied") {
-//       // We need to ask the user for permission
-//       Notification.requestPermission().then((permission) => {
-//         // If the user accepts, let's create a notification
-//         if (permission === "granted") {
-//           const notification = new Notification("Hi there!");
-//           // …
-//         }
-//       });
-//     }
-//   }
+function enviarN() {
+    addEventListener("click", () => {
+        let promise = Notification.requestPermission();
+        // wait for permission
+      });
+      
+    if (!("Notification" in window)) {
+        Notification.requestPermission();
+      // Check if the browser supports notifications
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification("Hi there!");
+      // …
+    } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          const notification = new Notification("Hi there!");
+          // …
+        }
+      });
+    }
+  }
   
-function enviarN(){
-    Notification.requestPermission();
-    self.addEventListener('push', (event) => {
-        event.waitUntil(
-          self.registration.showNotification('Notification Title', {
-            body: 'Notification Body Text',
-            icon: 'custom-notification-icon.png',
-          }));
-    });
-      };
+// function enviarN(){
+//     Notification.requestPermission();
+//     self.addEventListener('push', (event) => {
+//         event.waitUntil(
+//           self.registration.showNotification('Notification Title', {
+//             body: 'Notification Body Text',
+//             icon: 'custom-notification-icon.png',
+//           }));
+//     });
+//       };
       
