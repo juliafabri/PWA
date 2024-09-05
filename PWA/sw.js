2e21d326f058d35539f90459545184b45d4a1aca
juliafabri <137795108+juliafabri@users.noreply.github.com>
2024-09-05 17:30:35 -0300

    Add files via upload

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,15 +1,23 @@
-Notification.requestPermission();
-self.addEventListener('push', (event) => {
-    event.waitUntil(
-      self.registration.showNotification('Notificação', {
-        body: 'Isso é uma notificação!',
-        icon: 'custom-notification-icon.png',
-      }));
-);
-  });
+function enviarN() {
+    if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert("Esse browser não suporta notificações!");
+    } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+      const notification = new Notification("Isso é uma notificação!");
+      // …
+    } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          const notification = new Notification("Isso é uma notificação!");
+          // …
+        }
+      });
+    }
   
-  self.addEventListener('notificationclick', (event) => {
-    event.notification.close(); 
-    var fullPath = self.location.origin + event.notification.data.path; 
-    clients.openWindow(fullPath); 
-});
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them anymore.
+  }
