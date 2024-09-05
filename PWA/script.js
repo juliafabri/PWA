54eef5c83a299e9dcb10e51ff7781a65569d8dad
juliafabri <137795108+juliafabri@users.noreply.github.com>
2024-09-05 20:13:53 -0300

    Add files via upload

--- PWA/script.js
+++ PWA/script.js
@@ -1,22 +1,12 @@
 function enviarN() {
-    Notification.requestPermission().then(permission => {
-      if (permission === 'granted') {
-        console.log('Permission granted');
-      }
-    });
-  }
-
-  pedirPerm();
-
-  function enviarN() {
     if (Notification.permission === 'granted') {
       navigator.serviceWorker.getRegistration().then(reg => {
         const options = {
-          body: 'Isso é uma notificação!',
+          body: 'Essa é uma notificação!'
         };
-        reg.showNotification('Isso é uma notificação!', options);
+        reg.showNotification('Notificação PWA', options);
       });
     }
   }
 
-  enviarN();
+  enviarNotificacao();
